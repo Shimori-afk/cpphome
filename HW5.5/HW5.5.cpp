0df073ddb40237406bce0e2749a095e17ae0c4aa
Shimori-afk <maksym.leonovych@gmail.com>
2023-07-09 20:33:13 +0300

    Трохи переробив HW5.5 через так званий костиль але це працює.

--- HW5.5/HW5.5.cpp
+++ HW5.5/HW5.5.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
 #include <cctype>
+
 int main() {
-    char symbol;
-    int digitSum = 0;
+	char symbol;
+	int digitSum = 0;
 
-    while (true) {
-        std::cout << "Enter your element (To exit push'.'): ";
-        std::cin >> symbol;
+	while (true) {
+		std::cout << "Enter your element (To exit, push '.'): ";
+		std::cin >> symbol;
 
-        if (symbol == '.') {
-            std::cout << "Programm end." << std::endl;
-            break;
-        }
-        else if (islower(symbol)) {
-            char upperCaseSymbol = toupper(symbol);
-            std::cout << "From lowercase symbol to Uppercase: " << upperCaseSymbol << std::endl;
-        }
-        else if (isdigit(symbol)) {
-            int digit = symbol - '0';
-            digitSum += digit;
-            std::cout << "Sum of current numbers: " << digitSum << std::endl;
-        }
-        else {
-            std::cout << "This symbol don't work in this programm!" << std::endl;
-        }
-    }
+		if (symbol == '.') {
+			std::cout << "Program end." << std::endl;
+			break;
+		}
+		else if (islower(symbol)) {
+			char upperCaseSymbol = toupper(symbol);
+			std::cout << "From lowercase symbol to uppercase: " << upperCaseSymbol << std::endl;
+		}
+		else if (isdigit(symbol)) {
+			int digit = symbol - '0';
+			digitSum += digit;
+			std::cout << "Sum of current numbers: " << digitSum << std::endl;
+		}
+		else if (symbol == '+' || symbol == '-' || symbol == ',' || symbol == ';' || symbol == ':' || symbol == '*' || symbol == '/' || symbol == '^' || symbol == '$' || symbol == '%' || symbol == '!' || symbol == '@' || symbol == '#' || symbol == '&' || symbol == '(' || symbol == ')' || symbol == '[' || symbol == ']' || symbol == '{' || symbol == '}' || symbol == '?')
+		{
+			std::cout << "This symbol doesn't work in this program!" << std::endl;
+		}
+		else {
+			std::cout << "Error!" << std::endl;
+		}
+	}
 
-    return 0;
+	return 0;
 }
