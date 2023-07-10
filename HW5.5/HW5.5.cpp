cfad671c86896fb7e4355c3e6a948c9e1c97a286
Shimori-afk <maksym.leonovych@gmail.com>
2023-07-10 21:23:27 +0300

    Зробив зміни y HW5.4 i HW5.5

--- HW5.5/HW5.5.cpp
+++ HW5.5/HW5.5.cpp
@@ -1,33 +1,39 @@
 #include <iostream>
 #include <cctype>
 
-int main() {
+int main() 
+{
 	char symbol;
 	int digitSum = 0;
 
-	while (true) {
+	while (true)
+	{
 		std::cout << "Enter your element (To exit, push '.'): ";
 		std::cin >> symbol;
 
-		if (symbol == '.') {
+		if (symbol == '.')
+		{
 			std::cout << "Program end." << std::endl;
 			break;
 		}
-		else if (islower(symbol)) {
+		else if (islower(symbol)) 
+		{
 			char upperCaseSymbol = toupper(symbol);
 			std::cout << "From lowercase symbol to uppercase: " << upperCaseSymbol << std::endl;
 		}
-		else if (isdigit(symbol)) {
+		else if (isdigit(symbol)) 
+		{
 			int digit = symbol - '0';
 			digitSum += digit;
 			std::cout << "Sum of current numbers: " << digitSum << std::endl;
 		}
-		else if (symbol == '+' || symbol == '-' || symbol == ',' || symbol == ';' || symbol == ':' || symbol == '*' || symbol == '/' || symbol == '^' || symbol == '$' || symbol == '%' || symbol == '!' || symbol == '@' || symbol == '#' || symbol == '&' || symbol == '(' || symbol == ')' || symbol == '[' || symbol == ']' || symbol == '{' || symbol == '}' || symbol == '?')
+		else if (isupper(symbol))
+		{
+			std::cout << "This symbol is upper case and unavailable in this programm!" << std::endl;
+		}
+		else
 		{
 			std::cout << "This symbol doesn't work in this program!" << std::endl;
-		}
-		else {
-			std::cout << "Error!" << std::endl;
 		}
 	}
 
