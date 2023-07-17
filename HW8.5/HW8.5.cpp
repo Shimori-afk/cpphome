d6203fed316216b55eae71c77eeaf0dc2329aa87
Shimori-afk <maksym.leonovych@gmail.com>
2023-07-17 20:38:55 +0300

    Array task homework;

--- HW8.5/HW8.5.cpp
+++ HW8.5/HW8.5.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 bool isEqual(const char str1[], const char str2[])
 {
 	for (int i = 0; str1[i] != '\0' || str2[i] != '\0'; ++i)
@@ -7,7 +9,17 @@ bool isEqual(const char str1[], const char str2[])
 	}
 	return true;
 }
-int main()
-{
-	return  0;
+
+int main() {
+    const char str1[] = "Hello";
+    const char str2[] = "Hello";
+    const char str3[] = "World";
+
+    bool result1 = isEqual(str1, str2);
+    bool result2 = isEqual(str1, str3);
+
+    std::cout << "Result 1: " << (result1 ? "Equal" : "Not equal") << std::endl;
+    std::cout << "Result 2: " << (result2 ? "Equal" : "Not equal") << std::endl;
+
+    return 0;
 }
