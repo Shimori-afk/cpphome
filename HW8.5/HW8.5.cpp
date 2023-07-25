f7ede24f274f259ff29c84434815e430bbaa7225
Shimori-afk <maksym.leonovych@gmail.com>
2023-07-26 01:28:02 +0300

    some changes;

--- HW8.5/HW8.5.cpp
+++ HW8.5/HW8.5.cpp
@@ -2,18 +2,19 @@
 
 bool isEqual(const char str1[], const char str2[])
 {
-	for (int i = 0; str1[i] != '\0' || str2[i] != '\0'; ++i)
-	{
-		if (str1[i] != str2[i])
-			return false;
-	}
-	return true;
+    for (int i = 0; str1[i] != '\0' || str2[i] != '\0'; ++i)
+    {
+        if (str1[i] != str2[i])
+            return false;
+    }
+    return true;
 }
 
-int main() {
-    const char str1[] = "Hello";
-    const char str2[] = "Hello";
-    const char str3[] = "World";
+int main()
+{
+    const char str1[] = "abc";
+    const char str2[] = "abcde";
+    const char str3[] = "abc";
 
     bool result1 = isEqual(str1, str2);
     bool result2 = isEqual(str1, str3);
@@ -23,3 +24,4 @@ int main() {
 
     return 0;
 }
+
