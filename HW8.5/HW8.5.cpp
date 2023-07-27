5d8393bfb83edc69d93fe8bb98694a93090a2d1f
Shimori-afk <maksym.leonovych@gmail.com>
2023-07-27 20:18:29 +0300

    ...

--- HW8.5/HW8.5.cpp
+++ HW8.5/HW8.5.cpp
@@ -1,13 +1,25 @@
+#include <iostream>
+#include <string>
+
 bool isEqual(const char str1[], const char str2[])
 {
-	for (int i = 0; str1[i] != '\0' || str2[i] != '\0'; ++i)
+	if (strlen(str1) != strlen(str2))
 	{
-		if (str1[i] != str2[i])
-			return false;
+		return false;
 	}
 	return true;
 }
 int main()
 {
+	const char str1[] = "abc";
+	const char str2[] = "abcde";
+	const char str3[] = "abc";
+
+	bool result1 = isEqual(str1, str2);
+	bool result2 = isEqual(str1, str3);
+
+	std::cout << "Result 1: " << (result1 ? "Equal" : "Not equal") << std::endl;
+	std::cout << "Result 2: " << (result2 ? "Equal" : "Not equal") << std::endl;
+
 	return  0;
 }
