daccf7a837aea7303c60940ea587871ae1f87cd3
Shimori-afk <maksym.leonovych@gmail.com>
2023-08-15 19:41:46 +0300

    changed

--- HW10.3/HW10.3.cpp
+++ HW10.3/HW10.3.cpp
@@ -3,42 +3,45 @@
 
 void ColsSearch(int array[][4], int rows, int cols)
 {
-	for (int j = cols - 1; j >= 0; --j)
-	{
-		for (int i = 0; i < rows; ++i)
-		{
-			std::cout << array[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
+    for (int j = cols - 1; j >= 0; --j)
+    {
+        for (int i = 0; i < rows; ++i)
+        {
+            std::cout << array[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
 
 void RowsSearch(int array[][4], int rows, int cols)
 {
-	for (int i = rows - 1; i >= 0; --i)
-	{
-		for (int j = 0; j < cols; ++j)
-		{
-			std::cout << array[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = cols - 1; j >= 0; --j)
+        {
+            std::cout << array[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
 
 int main()
 {
-	const int rows = 3;
-	const int cols = 4;
-	int array2d[rows][cols] = {
-		{1, 2, 3, 4},
-		{5, 6, 7, 8},
-		{9, 10, 11, 12}
-	};
+    const int rows = 3;
+    const int cols = 4;
+    int array2d[rows][cols] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
 
-	ColsSearch(array2d, rows, cols);
+    std::cout << "ColsSearch:" << std::endl;
+    ColsSearch(array2d, rows, cols);
 
-	std::cout << std::endl;
+    std::cout << std::endl;
 
-	RowsSearch(array2d, rows, cols);
+    std::cout << "RowsSearch:" << std::endl;
+    RowsSearch(array2d, rows, cols);
 
+    return 0;
 }
